d1d66dae521e49ad1208ee2bec7fd17888df1b24
Jan Henrik Weinstock <jan.weinstock@rwth-aachen.de>
2019-04-19 23:48:19 +0200

    Cleanup

--- src/ftl/emitter.cpp
+++ src/ftl/emitter.cpp
@@ -21,10 +21,18 @@
 namespace ftl {
 
     enum opcode {
-        FTL_OPCODE_RET  = 0xc3,
+        OPCODE_RET  = 0xc3,
 
-        FTL_OPCODE_MOVI = 0xb8,
-        FTL_OPCODE_MOV  = 0x89,
+        OPCODE_MOVI = 0xb8,
+        OPCODE_MOV  = 0x89,
+    };
+
+    enum rexbits {
+        REX_BASE = 1 << 6,
+        REX_W    = 1 << 3, // whether operation is 64bit
+        REX_R    = 1 << 2, // whether the ModR/M REG field refers to r8-r15
+        REX_X    = 1 << 1, // whether the ModR/M SIB index refers to r8-r15
+        REX_B    = 1 << 0, // whether the ModR/M RM or SIB refers to r8-r15
     };
 
     emitter::emitter(cache& code):
@@ -36,16 +44,11 @@ namespace ftl {
     }
 
     size_t emitter::rex(bool is64, bool rexr, bool rexx, bool rexb) {
-        u8 rex = 0x40;
-        if (is64) // rex.w: whether operation is 64bit)
-            rex |= 8;
-        if (rexr) // rex.r: whether the ModR/M REG field refers to r8-r15
-            rex |= 4;
-        if (rexx) // rex.x: whether the ModR/M SIB index field refers to r8-r15
-            rex |= 2;
-        if (rexb) // rex.b: whether the ModR/M RM or SIB refers to r8-r15
-            rex |= 1;
-
+        u8 rex = REX_BASE;
+        if (is64) rex |= REX_W;
+        if (rexr) rex |= REX_R;
+        if (rexx) rex |= REX_X;
+        if (rexb) rex |= REX_B;
         return m_code.write(rex);
     }
 
@@ -60,27 +63,23 @@ namespace ftl {
     }
 
     size_t emitter::ret() {
-        m_code.write<u8>(FTL_OPCODE_RET);
+        m_code.write<u8>(OPCODE_RET);
         return sizeof(u8);
     }
 
     size_t emitter::movi(reg r, u64 imm) {
         size_t len = 0;
-
         len += rex(true, false, false, r >= REG_R8);
-        len += m_code.write<u8>(FTL_OPCODE_MOVI + (r & 0x7));
+        len += m_code.write<u8>(OPCODE_MOVI + (r & 0x7));
         len += m_code.write(imm);
-
         return len;
     }
 
     size_t emitter::mov(reg to, reg from) {
         size_t len = 0;
-
         len += rex(true, from >= REG_R8, false, to >= REG_R8);
-        len += m_code.write<u8>(FTL_OPCODE_MOV);
+        len += m_code.write<u8>(OPCODE_MOV);
         len += modrm(0b11, from & 7, to & 7);
-
         return len;
     }
 
