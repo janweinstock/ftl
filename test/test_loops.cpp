4656f2d7dc86558ff72f0dc6443585fae0b32050
Jan Henrik Weinstock <jan.weinstock@rwth-aachen.de>
2019-06-23 12:06:54 +0200

    Added unit test for loops

---
+++ test/test_loops.cpp
@@ -0,0 +1,66 @@
+/******************************************************************************
+ *                                                                            *
+ * Copyright 2019 Jan Henrik Weinstock                                        *
+ *                                                                            *
+ * Licensed under the Apache License, Version 2.0 (the "License");            *
+ * you may not use this file except in compliance with the License.           *
+ * You may obtain a copy of the License at                                    *
+ *                                                                            *
+ *     http://www.apache.org/licenses/LICENSE-2.0                             *
+ *                                                                            *
+ * Unless required by applicable law or agreed to in writing, software        *
+ * distributed under the License is distributed on an "AS IS" BASIS,          *
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   *
+ * See the License for the specific language governing permissions and        *
+ * limitations under the License.                                             *
+ *                                                                            *
+ ******************************************************************************/
+
+#include <gtest/gtest.h>
+
+#include "ftl.h"
+
+using namespace ftl;
+
+i64 append(void* ptr, i64 val) {
+    std::stringstream* ss = (std::stringstream*)ptr;
+    *ss << val;
+    return val;
+}
+
+TEST(loops, simple) {
+    i64 sum = 0;
+    std::stringstream ss;
+
+    cgen code(4 * KiB);
+    code.set_base_ptr(&ss);
+
+    func fn = code.gen_function();
+    value i = code.gen_local_i32(0);
+    value s = code.gen_global_i64(&sum);
+
+    label loop = code.gen_label();
+    loop.place();
+
+    value r = code.gen_call(append, i);
+    code.gen_add(s, r);
+    code.free_value(r);
+
+    code.gen_add(i, 1);
+    code.gen_cmp(i, 10);
+    code.gen_jl(loop);
+    code.gen_ret();
+
+    code.free_value(s);
+    code.free_value(i);
+
+    fn();
+
+    EXPECT_EQ(ss.str(), "0123456789");
+    EXPECT_EQ(sum, 45);
+}
+
+extern "C" int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
